de9d517659ac5afde9ed1b1900cc5fd975c33aa4
Aarush Maddula <100654110+AarushMaddula@users.noreply.github.com>
2024-08-26 17:23:56 -0400

    give user ability to change canvas dimensions

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
 const container = document.querySelector(".container");
 
-for (i = 0; i < 64; i++) {
-    const div = document.createElement("div");
-    div.setAttribute("class", "box")
-    div.addEventListener("mouseenter", (e) => {
-        e.target.style.backgroundColor = "black";
-    })
 
-    container.appendChild(div);
+function clearDivs() {
+    container.innerHTML = "";
 }
+
+function createDivs(length) {
+    for (i = 0; i < length ** 2; i++) {
+        const div = document.createElement("div");
+
+        div.setAttribute("class", "box");
+        div.style.width = `${800 / length}px`;
+        div.style.height = `${800 / length}px`;
+
+        div.addEventListener("mouseenter", (e) => {
+            e.target.style.backgroundColor = "black";
+        })
+
+        container.appendChild(div);
+    } 
+}
+
+
+const button = document.querySelector(".button");
+
+button.addEventListener("click", (e) => {
+    const num = prompt("How many squares per side? ");
+
+    if (+num === NaN || num > 100 || num < 1 || num % 1 !== 0) {
+        alert("Invalid Number")
+    } else {
+        clearDivs();
+        createDivs(num);
+    }
+
+})
+
+createDivs(8);
